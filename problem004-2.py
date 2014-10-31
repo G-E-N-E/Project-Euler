8898040aeadf58aab38530eeea18279d4b8ab8b0
Kenneth Wilkins <kwilkins@zoll.com>
2014-10-31 08:54:04 -0400

    Much better problem 4 solution

--- problem004-2.py
+++ problem004-2.py
@@ -3,9 +3,9 @@
 
 
 def l_p():
-    for a in xrange(9, 0, -1):
-        for b in xrange(9, 0, -1):
-            for c in xrange(9, 0, -1):
+    for a in xrange(9, 5, -1):
+        for b in xrange(9, -1, -1):
+            for c in xrange(9, -1, -1):
                 i = int(str(a)+str(b)+str(c)+str(c)+str(b)+str(a))
                 for j in xrange(999,100,-1):
                     if(i % j == 0  and i / j >= 100 and i / j < 999):
